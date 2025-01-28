272484719c056d6289102692ccb387479fbec114
Arina Nazarchuk <nazarchuk.arina@gmail.com>
2025-01-28 13:32:45 +0200

    a

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -19,8 +19,8 @@ export const getContactByIdController = async (req, res, next) => {
   const { contactId } = req.params;
   const contact = await getContactById(contactId);
 
-  if (!student) {
-    throw createHttpError(404, 'Student not found');
+  if (!contact) {
+    throw createHttpError(404, 'Contact not found');
   }
 
   res.status(200).json({
