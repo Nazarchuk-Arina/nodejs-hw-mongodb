e6dfe68fbb187ff3042ca0ac59ca5eff01bad05f
Arina Nazarchuk <nazarchuk.arina@gmail.com>
2025-02-08 21:07:11 +0200

    fff

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -2,7 +2,9 @@ import Joi from 'joi';
 
 export const createContactSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
-  phoneNumber: Joi.number().integer().min(6).max(16).required(),
+  phoneNumber: Joi.string()
+    .pattern(/^\+380\d{9}$/)
+    .required(),
   contactType: Joi.string().valid('work', 'home', 'personal').required(),
   email: Joi.string().email().required(),
   isFavourite: Joi.boolean(),
@@ -10,7 +12,7 @@ export const createContactSchema = Joi.object({
 
 export const updateContactSchema = Joi.object({
   name: Joi.string().min(3).max(20),
-  phoneNumber: Joi.number().integer().min(6).max(16),
+  phoneNumber: Joi.string().pattern(/^\+380\d{9}$/),
   contactType: Joi.string().valid('work', 'home', 'personal'),
   email: Joi.string().email(),
   isFavourite: Joi.boolean(),
